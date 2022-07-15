871e305848ae349f69e209b288b4e1ebe1f572be
GonzaloAlvrz <gonzalo.alvarez@arbusta.net>
2022-07-15 16:09:15 -0300

    Twitter

--- Actividad 2/twitter/src/components/Titulo.jsx
+++ Actividad 2/twitter/src/components/Titulo.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
+import LogoTwitter from '../img/twitter.png'
+import '../css/Titulo.css'
   class  Titulo extends React.Component{
     render(){
-        return  <div><h1>Twitter</h1></div>
+        return  <div><h1 className='titulo-titulo'><img src={LogoTwitter} alt='Logo twitter' className='logo-titulo'/>Twitter</h1></div>
     }
 }
 export default   Titulo;
